c7b19295c6839c14936eb663a38b1c3308b6c6a4
Chuck Lu <chucklu2015@outlook.com>
2021-05-27 15:46:28 +0800

    add test project app4

---
+++ eventLogNetFxSample/app4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using lib;
+
+namespace app4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var user in EventLogHelper.GetEventUsers())
+            {
+                Console.WriteLine(user);
+            }
+        }
+    }
+}
